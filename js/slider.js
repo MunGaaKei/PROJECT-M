f4dbf30bbac4971163de9f79023a4ec9a7a33084
MunGaaKei <502975759@qq.com>
2017-05-01 19:58:40 +0800

    slider - tiles

--- js/slider.js
+++ js/slider.js
@@ -46,38 +46,46 @@
 })();
 
 (function(){
-  var navs = $('ul.tiles-nav li'),
-      items = $('ul.tiles'),
-      bg = $('ul.tiles-bg li'),
-      item = items.children(),
-      i = 1, clock = true,
+  var navs = $('ul.tiles-a-nav li'),
+      items = $('ul.tiles-a'),
+      li = items.children('li'),
+      img = li.children(),
       images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'],
+      bg = $('#tiles-a-bg'),
+      i = 1, clock = true,
       len = images.length,
       tiles = function(){
         if(!clock) return false;
-        var j = 12, x, y;
         clock = false;
-        items.addClass('tiles-trans');
-        item.css('background-image', 'url("image/'+ images[i] +'")');
-        while(j--){
-          x = item[j].getAttribute('x');
-          y = item[j].getAttribute('y');
+        var j = 12, x, y;
+        items.addClass('tiles-a-trans');
+        img.attr('src', 'image/'+ images[i]);
+        navs.removeClass('active').eq(i).addClass('active');
+        while (j--) {
+          x = li[j].getAttribute('x');
+          y = li[j].getAttribute('y');
           (function(a, b, c){
             setTimeout(function(){
-              $(item[a]).css({'left': b, 'top': c});
+              $(li[a]).css({'left': b, 'top': c});
               if(a === 0){
                 setTimeout(function(){
                   clock = true;
-                  bg.css('background-image', 'url("image/'+ images[i-1 === -1?3: i-1] +'")');
-                  items.removeClass('tiles-trans');
-                  item.css({'left': '100%', 'top': '100%'});
+                  bg.attr('src', 'image/'+ images[i-1 === -1? len-1: i-1]);
+                  items.removeClass('tiles-a-trans');
+                  li.css({'left': '100%', 'top': '100%'});
                 }, 1500);
               }
-            }, a*120);
+            }, a * 120);
           })(j, x, y);
         }
         i = i < len-1? ++i: 0;
       },
       timer = setInterval(tiles, 3000);
-  bg.css('background-image', 'url("image/'+ images[0] +'")');
+  navs.on('click', function(){
+    if($(this).hasClass('active')) return false;
+    clearInterval(timer);
+    i = $(this).index();
+    tiles();
+    timer = setInterval(tiles, 3000);
+  });
 })();
